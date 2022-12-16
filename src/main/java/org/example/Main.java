08441dd177c41b36d87f7db660ded27710fa99a8
Alya <antsiperovich2002@mail.ru>
2022-12-16 22:36:03 +0300

    new1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> arrayList_2 = new ArrayList<>(); //123
+        ArrayList<String> arrayList_2 = new ArrayList<>(); //123 чсмсморло
         int size = 4;
         while (true) {
             System.out.println("\n----------Меню----------");
